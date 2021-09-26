b020977c88596e3372b89fe3b4d21c3611d55675
Eric Echemane <eechemane29@gmail.com>
2021-09-26 19:23:17 +0800

    added about me section

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,115 @@
 <script>
-	export let name;
+	import Content from './content.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<header class="shadow">
+		<h1> Rose Villaluna </h1>
+		<img class="avatar shadow" src="https://scontent.fmnl17-3.fna.fbcdn.net/v/t1.6435-9/241967965_3046291718993914_7971090436232087753_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFz8IE-EHu8zkHF6LPpdid5JiOvCmW8LRgmI68KZbwtGHmenBfbUQJZYa294Fap_5eyy9iAxIl4uu13ha8jDAOq&_nc_ohc=RyvjIGtK358AX9pqELA&_nc_ht=scontent.fmnl17-3.fna&oh=d987f887eed645d801d673037b441b99&oe=6176E1A1" alt="Rose Villaluna" />
+	</header>
+	<section class="outlined rounded">
+		<nav>
+			<a href="#about-me"> About me </a>
+			<a href="#about-my-course"> About my course </a>
+			<a href="#reflection"> Reflection on the course </a>
+			<a href="#prof"> Professor </a>
+		</nav>
+	</section>
+
+	<section class="outlined rounded" id="landing">
+		<div>
+			<h1> Hi, I am Rose Anne Villaluna. Thank for visiting my e-portfolio. </h1>
+			<br>
+			<p> Taking Bachelor of Science in Secondary Education Major in Social Studies in Pamantasan ng Lungsod ng Muntinlupa. </p>
+		</div>
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video class="shadow rounded" src="rose.mp4" autoplay controls loop></video>
+	</section>
+
+	<Content/>
 </main>
 
-<style>
-	main {
+	<style>
+	@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+
+	#landing {
+		display: flex;
+		gap: 2rem;
+		padding: 2rem;
+	}
+
+	#landing h1 {
+		font-size: 3rem;
+		background: linear-gradient(90deg,#ff0077, #c300ff, dodgerblue, #ff0077);
+		background-clip: text;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+
+	#landing video {
+		width: 50%;
+	}
+
+	nav {
+		display: flex;
+		gap: 1rem;
+	}
+	header {
+		width: 100%;
+		height: 32vh;
+		background: linear-gradient(90deg,#ff0077, #c300ff, dodgerblue, #ff0077);
+		background-size: 300%;
+		background-position: 0;
+		position: relative;
+		animation: shine 20s infinite;
+		margin-bottom: 7rem;
+		border-bottom: .25rem solid white;
+	}
+	header h1 {
+		color: white;
+		font-family: 'Lobster', cursive;
+		font-weight: 400;
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		padding: 1rem;
+		font-size: 5rem;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	header .avatar {
+		display: block;
+		position: relative;
+		width: 10rem;
+		height: 10rem;
+		border-radius: 50%;
+		border: .25rem solid white;
+		margin: auto;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+	section {
+		margin: 1rem;
+		padding: 1rem;
+		background-color: var(--surface1);
+	}
+	@media (max-width: 560px) {
+		header h1 {
+			font-size: 3rem;
+			top: 1rem;
+		}
+		header {
+			height: 25vh;
+		}
+		nav {
+			flex-direction: column;
+		}
+		#landing {
+			flex-direction: column;
+			padding: 1rem;
+		}
+		#landing h1 {
+			font-size: 1.5rem;
+		}
+		#landing h1, #landing p {
+			text-align: center;
+		}
+		#landing video {
+			width: 100%;
 		}
 	}
 </style>
---
+++ src/content.svelte
@@ -0,0 +1,37 @@
+<article class="outlined" id="about-me">
+    <h1> About me </h1>
+    <p> 
+        Good Day everyone! My name is Rose Anne H. Villaluna taking Bachelor of Science in Secondary education major in Social Studies. I have a twin sister her name is Remy Anne H. Villaluna taking Bachelor of Science in Information Technology. We are currently living here in Sta Cruz Occidental Mindoro together with our parents. I was good in school and I received a lot of certificates from different subjects. My favorite colors are purple and yellow. I love helping and involving in school activities. Having a great experience here in our province together with our family and relatives is such a wonderful feeling ❤️. My tatay is a farmer and my mama is a house wife who taught us to be a good person. I do love my friends in CITCS department because there are the one who cheer me up to do what I really want in life.
+    </p>
+    <section>
+        <img src="rosetrophy.jpg" alt="" height="400px" loading="lazy">
+        <img src="rosepink.jpg" alt="" height="400px" id="rosepink" loading="lazy">
+    </section>
+</article>
+
+<style>
+    article {
+        background-color: var(--surface1);
+        margin: 1rem;
+        padding: 1rem;
+    }
+    article p {
+        margin-top: 1rem;
+    }
+    article section {
+        display: flex;
+        margin: 1rem 0;
+        gap: 1rem;
+    }
+    img {
+        border-radius: .5rem;
+    }
+    @media (max-width: 800px) {
+        article section {
+            flex-direction: column;
+        }
+        #rosepink {
+            height: auto;
+        }
+    }
+</style>
